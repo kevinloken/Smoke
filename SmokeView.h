73dc1ee8f1555a6c9670e51717f808e7354c6b1a
Kevin Loken <kevinloken@stonesanctuaryinteractive.com>
2011-02-18 10:32:03 -0800

    Initial commit

---
+++ SmokeView.h
@@ -0,0 +1,17 @@
+//
+//  SmokeView.h
+//  Smoke
+//
+//  Created by Kevin Loken on 11-02-18.
+//  Copyright (c) 2011, Stone Sanctuary Interactive Inc. All rights reserved.
+//
+
+#import <ScreenSaver/ScreenSaver.h>
+
+
+@interface SmokeView : ScreenSaverView 
+{
+
+}
+
+@end
---
+++ SmokeView.m
@@ -0,0 +1,53 @@
+//
+//  SmokeView.m
+//  Smoke
+//
+//  Created by Kevin Loken on 11-02-18.
+//  Copyright (c) 2011, Stone Sanctuary Interactive Inc. All rights reserved.
+//
+
+#import "SmokeView.h"
+
+
+@implementation SmokeView
+
+- (id)initWithFrame:(NSRect)frame isPreview:(BOOL)isPreview
+{
+    self = [super initWithFrame:frame isPreview:isPreview];
+    if (self) {
+        [self setAnimationTimeInterval:1/30.0];
+    }
+    return self;
+}
+
+- (void)startAnimation
+{
+    [super startAnimation];
+}
+
+- (void)stopAnimation
+{
+    [super stopAnimation];
+}
+
+- (void)drawRect:(NSRect)rect
+{
+    [super drawRect:rect];
+}
+
+- (void)animateOneFrame
+{
+    return;
+}
+
+- (BOOL)hasConfigureSheet
+{
+    return NO;
+}
+
+- (NSWindow*)configureSheet
+{
+    return nil;
+}
+
+@end
